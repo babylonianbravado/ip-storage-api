991cf3c0802eef984a34760b5538471f4b97ffd2
𒉿𒊏 𒀉𒋼𒅕 <babylonianbravado@tutanota.com>
2023-09-25 16:15:10 +0200

    redis caching was added

--- src/api.module.ts
+++ src/api.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import type { RedisClientOptions } from 'redis';
+import * as redisStore from 'cache-manager-redis-store';
+
+import { REDIS_HOST, REDIS_PORT } from 'utils/constants';
 
 import { ApiController } from './api.constroller';
 import { ApiService } from './api.service';
 
 @Module({
+  imports: [
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory:
+        async (configService: ConfigService) => <RedisClientOptions>({
+          isGlobal: true,
+          store: redisStore,
+          host: configService.get(REDIS_HOST),
+          port: configService.get(REDIS_PORT)
+        }),
+      inject: [ConfigService],
+    })
+  ],
   controllers: [ApiController],
   providers: [ApiService],
 })
-export class ApiModule {}
+export class ApiModule { }
